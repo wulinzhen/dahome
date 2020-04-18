6b46f5facc75aa4b5a361d707cf573f7448a90d5
wulinzhen <891449056@qq.com>
2020-04-18 23:41:07 +0800

    fisrstcommit

---
+++ dahome/src/main/java/com/wisdom/dahome/DahomeApplication.java
@@ -0,0 +1,13 @@
+package com.wisdom.dahome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DahomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DahomeApplication.class, args);
+	}
+
+}
---
+++ dahome/src/test/java/com/wisdom/dahome/DahomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisdom.dahome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DahomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
